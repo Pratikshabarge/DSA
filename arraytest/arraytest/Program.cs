144af84976c47320d4095e847827aa643f851059
Pratikshabarge <pratikshab1994@gmail.com>
2022-08-22 21:12:57 +0530

    Second Largest element inn array

---
+++ arraytest/arraytest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace arraytest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 45, 56, 2, 8, 9, 46, 90,898 };
+            int temp= arr[0];
+            int t2=0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if ( arr[i] > temp )
+                {
+                    if ( temp> t2 )
+                    {
+                        t2 = temp;
+                    }
+                   
+                    temp = arr[i];
+                }
+            }
+            Console.WriteLine("second largest number: {0}", t2);
+            Console.ReadLine();
+
+        }
+    }
+}
